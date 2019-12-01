132cc73eafcb5e82440844abb6aae1a4294add3f
Ycmelon <lohyuchen12345@gmail.com>
2019-12-01 20:59:56 +0800

    init canvas support

--- main.py
+++ main.py
@@ -10,31 +10,61 @@ from urllib.request import urlopen, urlretrieve, Request
 
 # TODO: log
 # TODO: migrate urllib to requests
+# TODO: make into module, web api
+
+
+def is_canvas(title_no):
+    # Not canvas
+    output = False
+    url = f"https://www.webtoons.com/en/fantasy/castle-swimmer/extra-episode-3/viewer?title_no={title_no}&episode_no=1"
+
+    # Canvas
+    if requests.get(url).status_code == 404:
+        url = f"https://www.webtoons.com/en/challenge/castle-swimmer/extra-episode-3/viewer?title_no={title_no}&episode_no=1"
+        output = True
+
+    # Error
+    if requests.get(url).status_code == 404:
+        output = False
+        raise Exception("Webtoon not found!")
+
+    return output
 
 
 def loading_bar(iterable, unit):
     return tqdm(iterable, unit=unit, ncols=100)
 
 
-def get_page_title(url):
-    webpage = urlopen(url).read()
-    title = str(webpage).split('<title>')[1].split('</title>')[0]
-    return title
+def get_title(url):
+    if not canvas:
+        webpage = urlopen(url).read()
+        title = str(webpage).split('<title>')[1].split('</title>')[0]
+        return title
+    else:
+        # TODO: get title name for canvas
+        title = title_no
+        return title
 
 
 def get_full_url(title_no, episode_no):
-    # TODO: add canvas support (fantasy --> challenge)
-    return f"https://www.webtoons.com/en/fantasy/castle-swimmer/extra-episode-3/viewer?title_no={title_no}&episode_no={episode_no}"
+    if not canvas:
+        url = f"https://www.webtoons.com/en/fantasy/castle-swimmer/extra-episode-3/viewer?title_no={title_no}&episode_no={episode_no}"
+    else:
+        url = f"https://www.webtoons.com/en/challenge/castle-swimmer/extra-episode-3/viewer?title_no={title_no}&episode_no={episode_no}"
+
+    return url
 
 
-webtoon_filetype = "jpg"  # if changed in future
-bs4_htmlparser = "html.parser"
-download_range = list(range(1, 46))
+# Variables
+download_range = list(range(1, 3))
 pdf = True
-title_no = "1499"
-title = get_page_title(get_full_url(title_no, "1")).split(" | ")[1]
+title_no = "219164"
+canvas = is_canvas(title_no)
+title = get_title(get_full_url(title_no, "1")).split(" | ")[1]
 document_name = f"{title} Episodes {download_range[0]}-{download_range[-1]}"
 working_dir = os.path.join("output/", document_name)
+webtoon_filetype = "jpg"  # if changed in future
+bs4_htmlparser = "html.parser"
 request_headers = {'User-agent': 'Mozilla/5.0',
                    "Referer": get_full_url(title_no, "1")}
 
@@ -57,6 +87,7 @@ for episode_no in loading_bar(download_range, "links"):
         image_urls.append(img.get("data-url"))
 # TODO: save progress
 
+
 # Saving files
 for index, image_url in loading_bar(enumerate(image_urls), "images"):
     filename = f"{index}.{webtoon_filetype}"
@@ -71,8 +102,9 @@ for index, image_url in loading_bar(enumerate(image_urls), "images"):
         pass
 
 
-# Saving PDFs
+# Saving PDF
 if pdf:
+    # TODO: individual PDFs for each ep
     file_paths = glob.glob(f"{working_dir}/*.{webtoon_filetype}")
     with open(os.path.join(working_dir, f"{document_name}.pdf"), "wb") as file:
         file.write(img2pdf.convert(file_paths))
