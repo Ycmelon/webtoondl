1f6b66383188d391045f4adac28c236cc28046a9
Ycmelon <lohyuchen12345@gmail.com>
2019-12-01 20:24:54 +0800

    revamp

--- main.py
+++ main.py
@@ -1,60 +1,78 @@
+import os
+import glob
+import shutil
+import img2pdf
+import requests
+import urllib.parse as urlparse
+from tqdm import tqdm
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, urlretrieve, Request
-import urllib.parse as urlparse
-import requests, shutil, os, img2pdf
-from tqdm import tqdm
 
-urlinput = []
-urlform = ["https://www.webtoons.com/en/fantasy/refundhighschool/ep-","/viewer?title_no=1360&episode_no="]
-for i in range(1,103):
-    urlinput.append(urlform[0]+str(i)+urlform[1]+str(i))
+# TODO: log
+# TODO: migrate urllib to requests
 
-doctitle = "rhs 1-103 test"
-urls = []
-filenames = []
 
-if not os.path.exists(doctitle):
-    os.makedirs(doctitle)
+def loading_bar(iterable, unit):
+    return tqdm(iterable, unit=unit, ncols=100)
 
-print("Step 1: Getting image urls")
-for url in tqdm(urlinput, unit="links", ncols=100):
-    soup = BeautifulSoup(urlopen(url).read(),features="html.parser")
-    parser = urlparse.parse_qs(urlparse.urlparse(url).query)
 
-    for i in soup.find_all("div"):
-        if i.get("id"):
-            if i.get("id") == "_imageList":
-                for a in i.find_all("img"):
-                    urls.append(a.get("data-url"))
-                    #print("getting: "+str(len(urls)))
+def get_page_title(url):
+    webpage = urlopen(url).read()
+    title = str(webpage).split('<title>')[1].split('</title>')[0]
+    return title
 
-    with open(doctitle+"/"+doctitle+".txt","a+") as file:
-        for i in urls:
-            if urls.index(i) == len(urls)-1:
-                file.write(i)
-            else:
-                file.write(i+"\n")
 
-os.system("cls")
-print("Step 2: Saving files")
-for i in tqdm(urls, unit="images", ncols=100):
-    index = urls.index(i)
-    filename = doctitle+"/"+doctitle+"-"+str(index)+".jpg"
-    try:
-        r = requests.get(i, stream=True, headers={'User-agent': 'Mozilla/5.0', "Referer": urlinput[0]})
-        if r.status_code == 200:
-            with open(filename, 'wb') as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-    except Exception as error:
-        print("Error saving "+filename)
-        print("Error: "+error)
+def get_full_url(title_no, episode_no):
+    # TODO: add canvas support (fantasy --> challenge)
+    return f"https://www.webtoons.com/en/fantasy/castle-swimmer/extra-episode-3/viewer?title_no={title_no}&episode_no={episode_no}"
+
+
+webtoon_filetype = "jpg"  # if changed in future
+bs4_htmlparser = "html.parser"
+download_range = list(range(1, 46))
+pdf = True
+title_no = "1499"
+title = get_page_title(get_full_url(title_no, "1")).split(" | ")[1]
+document_name = f"{title} Episodes {download_range[0]}-{download_range[-1]}"
+working_dir = os.path.join("output/", document_name)
+request_headers = {'User-agent': 'Mozilla/5.0',
+                   "Referer": get_full_url(title_no, "1")}
+
+
+if os.path.exists(working_dir):
+    # Resume progress
+    # TODO: progress saving info file
+    pass
+else:
+    os.makedirs(working_dir)
+
+
+# Getting image URLs
+image_urls = []
+for episode_no in loading_bar(download_range, "links"):
+    url = get_full_url(title_no, episode_no)
+    soup = BeautifulSoup(urlopen(url).read(), features=bs4_htmlparser)
+
+    for img in soup.find(id="_imageList").find_all("img"):
+        image_urls.append(img.get("data-url"))
+# TODO: save progress
+
+# Saving files
+for index, image_url in loading_bar(enumerate(image_urls), "images"):
+    filename = f"{index}.{webtoon_filetype}"
+
+    request = requests.get(image_url, stream=True, headers=request_headers)
+    if request.status_code == 200:
+        with open(os.path.join(working_dir, filename), 'wb') as file:
+            request.raw.decode_content = True
+            shutil.copyfileobj(request.raw, file)
     else:
-        filenames.append(filename)
-        #print("saved: "+str(len(filenames)))
+        # TODO: error handling, tryagain
+        pass
 
-print("Saving pdf...")
-with open(doctitle+"/"+doctitle+".pdf", "wb") as f:
-    f.write(img2pdf.convert(filenames))
-print("Successfully saved pdf! Press any key to exit.")
-os.system("pause >nul")
+
+# Saving PDFs
+if pdf:
+    file_paths = glob.glob(f"{working_dir}/*.{webtoon_filetype}")
+    with open(os.path.join(working_dir, f"{document_name}.pdf"), "wb") as file:
+        file.write(img2pdf.convert(file_paths))
